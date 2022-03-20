0c70a35bd2658955bf31b8a2fed7a98d7b35c95f
nick0der <mykolarpopyk@gmail.com>
2022-03-20 18:37:44 +0200

    First commit

--- locust.py
+++ locust.py
@@ -1,19 +1,22 @@
 import time
-import random
 import json
+import random
 from locust import HttpUser, task, between
 
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 5)
-    
+
+    #wait_time = between(1, 2)
+
     @task(1)
     def get_cars(self):
         self.client.get("/cars")
 
     @task(1)
     def get_car_by_id(self):
-        req="/cars/"+self.client.get("/cars").json()[0]["_id"]
+        cars = self.client.get("/cars").json()
+        req="/cars/"+cars[random.randint(1,len(cars)-1)]["_id"]
+        #req="/cars/"+self.client.get("/cars").json()[0]["_id"]
         self.client.get(req)
 
     @task(1)
@@ -31,6 +34,8 @@ class WebsiteUser(HttpUser):
 
     @task(1)
     def put_cars(self):
+        #cars = self.client.get("/cars").json()
+        #req="/cars/"+cars[random.randint(1,len(cars)-1)]["_id"]
         req="/cars/"+self.client.get("/cars").json()[-1]["_id"]
 
         self.client.put(req, json={
@@ -47,8 +52,7 @@ class WebsiteUser(HttpUser):
     @task(1)
     def delete_car(self):
         cars = self.client.get("/cars").json()
-        req="/cars/"+self.client.get("/cars").json()[len(cars)-(len(cars)/2)]["_id"]
+        #req="/cars/"+self.client.get("/cars").json()[-1]["_id"]
+        req="/cars/"+cars[random.randint(1,len(cars)-1)]["_id"]
 
-        self.client.delete(req)        
-            
-                
+        self.client.delete(req)
